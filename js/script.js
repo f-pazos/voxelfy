c9475d1f8fd087b21b28097ecc5b17ab55eb2693
Felipe Pazos <gadzookese@gmail.com>
2016-08-15 10:57:56 -0400

    Implemented stock three.js cube rendering.

--- js/script.js
+++ js/script.js
@@ -35,13 +35,39 @@ function handleFileSelect( evt ){
   	var buf = Buffer.from( arrayBuffer );
 
   	mesh = parseSTL( buf );
-  	console.log( "mesh updated." );
+  	console.log( mesh );
 	}
 
 	reader.readAsArrayBuffer( file );
-
-
 }
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshNormalMaterial( {color: 0x00ff00 } );
+
+
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function render(){
+
+	requestAnimationFrame( render );
+
+	cube.rotation.x += 0.03;
+	cube.rotation.y += 0.03;
+
+	renderer.render( scene, camera );
+};
+
+render();
 
 /*var buf = fs.readFileSync('mesh.stl');
 var mesh = parseSTL(buf);*/
