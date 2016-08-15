c2a80a8bc4aa5c41706f5d5c1206c35a38dc62c5
Felipe Pazos <gadzookese@gmail.com>
2016-08-15 10:18:35 -0400

    Created a file select that successfully parses the STL file and creates a JS object with the STL data.

--- js/script.js
+++ js/script.js
@@ -5,10 +5,49 @@
 'use strict';
 
 var parseSTL = require('parse-stl');
-var fs = require('fs');
+/*var fs = require( 'fs' )
 
-var buf = fs.readFileSync('mesh.stl');
-var mesh = parseSTL(buf);
+var buf = fs.readFileSync( 'mesh.stl' );
 
-console.log(mesh);
+console.log( buf );
+
+var mesh = parseSTL( buf );*/
+
+//Set JS to handleFileSelect when file input changes.
+document.getElementById( 'files' ).addEventListener( 'change', 
+	handleFileSelect, false);
+
+
+function handleFileSelect( evt ){
+	var files = evt.target.files; //FileList object
+	var file = files[0]; //Select First File
+
+	console.log( file );
+
+	//Read in file as a binary string.
+	var reader = new FileReader();
+
+	reader.onload = function(e) {
+  	var arrayBuffer = reader.result;
+  	console.log( 'e', arrayBuffer );
+
+  	var buf = Buffer.from( arrayBuffer );
+  	console.log( 'f', buf );
+
+  	var mesh = parseSTL( buf );
+  	console.log( 'd', mesh );
+
+	}
+
+	reader.readAsArrayBuffer( file );
+
+	console.log( 'a', reader );
+	
+	console.log( 'b', reader.result );
+
+}
+
+/*var buf = fs.readFileSync('mesh.stl');
+var mesh = parseSTL(buf);*/
+
 
