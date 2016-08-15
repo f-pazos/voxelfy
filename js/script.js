d6d54d94e8e2266de7d3e8db954fe2b4ac437eb0
Felipe Pazos <gadzookese@gmail.com>
2016-08-15 08:17:17 -0400

    Installed browserify.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@
 //File to take a provided STL file, and convert it to a voxel representation.
 
 'use strict';
+
+var parseSTL = require('parse-stl');
+var fs = require('fs');
+
+var buf = fs.readFileSync('mesh.stl');
+var mesh = parseSTL(buf);
+
+console.log(mesh);
+
